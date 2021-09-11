75858237cc5bbdb21059435ded250cc080c65d42
Jhon Serna <jhonalfaber@gmail.com>
2021-09-11 18:49:28 -0500

    Implementando correctamente el metodo calculateBalance en Account.java

--- src/main/java/entitys/Account.java
+++ src/main/java/entitys/Account.java
@@ -26,8 +26,14 @@ public class Account {
         this.debit += debit;
     }
 
-    private void calculateBalance() {
-        this.balance = this.credit-this.debit;
+    private void calculateBalance(ArrayList<Account> db) {
+        int size = db.size();
+        if (size > 0) {
+            Account lastData = db.get(size - 1);
+            balance += lastData.getBalance();
+        }
+        balance += this.credit;
+        balance -= this.debit;
     }
 
     private void setDate(LocalDate date) {
@@ -56,7 +62,7 @@ public class Account {
         DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         LocalDate localDate = LocalDate.parse(date,df);
         account.setDate(localDate);
-        account.calculateBalance();
+        account.calculateBalance(db);
         db.add(account);
     }
 
@@ -66,7 +72,7 @@ public class Account {
         DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         LocalDate localDate = LocalDate.parse(date,df);
         account.setDate(localDate);
-        account.calculateBalance();
+        account.calculateBalance(db);
         db.add(account);
     }
 
